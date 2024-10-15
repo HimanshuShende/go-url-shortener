3113e0c48e220fd09ba47ed35f844044a370b228
Himanshu Shende <himanshu.shende9(@gmail.com>
2024-10-16 01:18:11 +0530

    changed r to router in main.go

--- main.go
+++ main.go
@@ -9,25 +9,25 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Welcome to the URL Shortener API",
 		})
 	})
 
-	r.POST("/create-short-url", func(ctx *gin.Context) {
+	router.POST("/create-short-url", func(ctx *gin.Context) {
 		handler.CreateShortUrl(ctx)
 	})
 
-	r.GET("/:shortUrl", func(ctx *gin.Context) {
+	router.GET("/:shortUrl", func(ctx *gin.Context) {
 		handler.HandleShortIrlRedirect(ctx)
 	})
 
 	// Store initialization
 	store.InitializeStore()
 
-	err := r.Run(":9808")
+	err := router.Run(":9808")
 
 	if err != nil {
 		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
